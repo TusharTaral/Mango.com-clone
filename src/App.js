fc46091135eb4a8e284721aaece31ab7b346f440
tushar taral <tushartaral0@gmail.com>
2021-05-19 14:25:27 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 import Navbar from './Components/Navbar/Navbar';
 import FrontPage from './Components/FrontPage/FrontPage';
 import SelectOption from './Components/Option_page/SelectOption';
+import MenPage from './Components/MenPage/MenPage';
+import WomenPage from './Components/WomenPage/WomenPage';
+import KidsPage from './Components/KidsPage/KidsPage';
+import PlusSize from './Components/PlusSize/PlusSize';
 
 
 function App() {
   return (
     <div className="App">
-        {/* <Navbar/>  */}
-        {/* <FrontPage/> */}
-        <SelectOption/>
+      {/* <Navbar /> */}
+      {/* <MenPage /> */}
+      <WomenPage />
+      {/* <KidsPage /> */}
+      {/* <PlusSize /> */}
+      {/* <FrontPage /> */}
+      {/* <SelectOption /> */}
     </div>
   );
 }
